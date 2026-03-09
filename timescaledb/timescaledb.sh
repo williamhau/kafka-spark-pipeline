169d1dbdec61d5ce1f054d8f49bed699b029b2d8
whau <williamhau@yahoo.com>
2026-03-08 20:53:38 -0400

    add timescaledb

---
+++ timescaledb/timescaledb.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -a
+source ../.env
+set +a
+
+# TimescaleDB
+# 1. Start in background
+docker  compose -f docker-timescaledb-compose.yml up -d
+
+# 2. Look at logs
+docker compose -f docker-timescaledb-compose.yml logs -f
+
+# 3. Stop and remove containers
+docker  compose -f docker-timescaledb-compose.yml down
